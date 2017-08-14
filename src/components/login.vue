3c5e9ec1524c562312af27332608d6e05d11beea
zhangchi <zhangchi@corp-ci.com>
2017-08-14 14:12:53 +0800

    增加elment-ui的使用

--- src/components/login.vue
+++ src/components/login.vue
@@ -10,6 +10,9 @@
       <input name="password" v-model="password">
     </div>
     <button name="btn" value="summit">summit</button>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
   </div>
 </template>
   
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+Vue.use(ElementUI)
+
 new Vue({
   el: '#app',
   router,

5af75fcbc120512c457d677e8d43f64a4de87ba3
latelater <903430068@qq.com>
2017-08-19 16:39:31 +0800

    添加组件悬浮效果

--- src/components/common/article.vue
+++ src/components/common/article.vue
@@ -1,30 +1,20 @@
 <template>
   <div id="Article">
-    <el-row class="margin_top">
-      <el-col :span="12" :offset="3" class="row-bg grid-content">
-        <el-col :offset="1">
-          <h2>我是titile</h2>
-          <el-tag type="gray">作者：zhangchi</el-tag>
-          <p>2017-08-15 13:00</p>
-          <p>上面的脚本最终也会alert 16，因为虽然bar不直接处于foo的内部作用域，但bar还是能访问x和tmp。但是，由于tmp仍存在于bar闭包的内部，所以它还是会自加1，而且你每次调用bar时它都会自加1.
-　　（考虑到六岁这个限制：我们其实可以建立不止一个闭包方法，比如return它们的数组，也可以把它们设置为全局变量。它们全都指向相同的x和相同的tmp，而不是各自有一份副本。）
-　　注：现在来整点儿七岁的内容。上面的x是一个字面值（值传递），和JS里其他的字面值一样，当调用foo时，实参x的值被复制了一份，复制的那一份作为了foo的参数x。
-　　那么问题来了，JS里处理object时是用到引用传递的，那么，你调用foo时传递一个object，foo函数return的闭包也会引用最初那个object！
-          </p>
-        </el-col>
-      </el-col>
-    </el-row>
-
-    <el-row class="margin_top">
-      <el-col :span="12" :offset="3" class="row-bg grid-content">
-          <h2>我是titile</h2>
-          <el-tag type="gray">作者：zhangchi</el-tag>
-          <p>2017-08-15 13:00</p>
-          <p>上面的脚本最终也会alert 16，因为虽然bar不直接处于foo的内部作用域，但bar还是能访问x和tmp。但是，由于tmp仍存在于bar闭包的内部，所以它还是会自加1，而且你每次调用bar时它都会自加1.
-　　（考虑到六岁这个限制：我们其实可以建立不止一个闭包方法，比如return它们的数组，也可以把它们设置为全局变量。它们全都指向相同的x和相同的tmp，而不是各自有一份副本。）
-　　注：现在来整点儿七岁的内容。上面的x是一个字面值（值传递），和JS里其他的字面值一样，当调用foo时，实参x的值被复制了一份，复制的那一份作为了foo的参数x。
-　　那么问题来了，JS里处理object时是用到引用传递的，那么，你调用foo时传递一个object，foo函数return的闭包也会引用最初那个object！
-          </p>
+    <el-row>
+      <el-col 
+      :span="12" :offset="3" class="row-bg"
+      :class="{'grid-content': isShow}"
+      :mouseover="showBorder()" 
+      :mousemove="hideBoder()"
+      :mouseup="pageRedirect()">
+        <h2>我是titile</h2>
+        <el-tag type="gray">作者：zhangchi</el-tag>
+        <p>2017-08-15 13:00</p>
+        <p>上面的脚本最终也会alert 16，因为虽然bar不直接处于foo的内部作用域，但bar还是能访问x和tmp。但是，由于tmp仍存在于bar闭包的内部，所以它还是会自加1，而且你每次调用bar时它都会自加1.
+　　（考虑到六岁这个限制：我们其实可以建立不止一个闭包方法，比如return它们的数组，也可以把它们设置为全局变量。它们全都指向相同的x和相同的tmp，而不是各自有一份副本。）注：现在来整点儿七岁的内容。上面的x是一个字面值（值传递），和JS里其他的字面值一样，当调用foo时，实参x的值被复制了一份，复制的那一份作为了foo的参数x。那么问题来了，JS里处理object时是用到引用传递的，那么，你调用foo时传递一个object，foo函数return的闭包也会引用最初那个object！
+     上面的脚本最终也会alert 16，因为虽然bar不直接处于foo的内部作用域，但bar还是能访问x和tmp。但是，由于tmp仍存在于bar闭包的内部，所以它还是会自加1，而且你每次调用bar时它都会自加1.
+　　（考虑到六岁这个限制：
+        </p>
       </el-col>
     </el-row>
   </div>
@@ -35,25 +25,50 @@
     name: "Article",
     data() {
       return {
-
+        isShow: false
       }
     },
-    methods: {},
+    methods: {
+      showBorder() {
+        this.isShow = true;
+        console.log("this.isShow = true;");
+      },
+      hideBoder() {
+        console.log("this.isShow = false;");
+        this.isShow = false;
+      },
+      pageRedirect() {
+        console.log("pageRedirect")
+      }
+    },
     components: {}
   }
 </script>
 
 <style>
+  #Article {
+    line-height:22px;
+    margin-top: 40px;
+    
+  }
   .grid-content {
-    border-radius: 4px;
     min-height: 36px;
+		border-width: 1px;
+		border-style: outset;
+		border-radius: 4px;
+		/* border-color: #efefef;  */
+		box-shadow:2px 2px 3px #aaaaaa;
   }
   .row-bg {
-    padding: 10px 0;
+    padding: 10px 40px;
     background-color: #f9fafc;
   }
-  .margin_top {
-    margin-top: 40px;
+  .boder-big:{
+		border-width: 1px;
+		border-style: outset;
+		border-radius: 4px;
+		/* border-color: #efefef;  */
+		box-shadow:2px 2px 3px #aaaaaa;
   }
 </style>
 
